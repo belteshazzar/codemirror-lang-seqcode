90c5719d7f6a0dcdcc30bf36cb6c07b7a1f9e8a4
Daniel Walton <daniel@belteshazzar.com.au>
2025-05-14 00:51:37 +0930

    working

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,10 @@
-import typescript from "rollup-plugin-ts"
+
+
 import {lezer} from "@lezer/generator/rollup"
 
 export default {
-  input: "src/index.ts",
-  external: id => id != "tslib" && !/^(\.?\/|\w:)/.test(id),
-  output: [
-    {file: "dist/index.cjs", format: "cjs"},
-    {dir: "./dist", format: "es"}
-  ],
-  plugins: [lezer(), typescript()]
+  input: "src/index.js",
+  output: 
+    {dir: "./dist", format: "es",importAttributesKey: "assert"},
+  plugins: [lezer()]
 }
